2ae5d9643b0dfb3031356abb840819afe3557ca2
Josh Myers <joshuajmyers@gmail.com>
2017-03-17 13:43:51 +0000

    Drop RUN_ZAP_TESTS from library
    We always run ZAP_TESTS as part of a pipeline and the run-end-to-end-tests defaults to this behaviour.

--- vars/runEndToEnd.groovy
+++ vars/runEndToEnd.groovy
@@ -9,7 +9,6 @@ def call(String app, String tag = null) {
     build job: 'run-end-to-end-tests',
         parameters:[
           string(name: 'MODULE_NAME', value: app),
-          string(name: 'MODULE_TAG', value: tag),
-          booleanParam(name: 'RUN_ZAP_TESTS', value: env.RUN_ZAP_TESTS.toBoolean())
+          string(name: 'MODULE_TAG', value: tag)
         ]
 }
