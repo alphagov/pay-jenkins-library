240dfc7fcd1675bc570bfc6f562e532b9c1e7f81
Alex Bishop <alexbishop1@users.noreply.github.com>
2017-04-04 12:38:55 +0100

    PP-1866 Add param for TAG_AFTER_DEPLOYMENT to deploy function
    Sets the TAG_AFTER_DEPLOYMENT argument to true or false when
    calling the deploy-pipeline-microservice job
    
    Defaults to false if not provided
    
    with @mrlumbu

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String microservice, String aws_profile, String tag = null) {
+def call(String microservice, String aws_profile, String tag = null, boolean tagAfterDeployment = false) {
     tag = tag ?: "latest-master"
     
     build job: 'deploy-pipeline-microservice',
@@ -8,5 +8,6 @@ def call(String microservice, String aws_profile, String tag = null) {
           string(name: 'MICROSERVICE', value: microservice),
           string(name: 'CONTAINER_VERSION', value: tag),
           string(name: 'AWS_PROFILE', value: aws_profile),
+          booleanParam(name: 'TAG_AFTER_DEPLOYMENT', value: tagAfterDeployment),
         ]
 }
