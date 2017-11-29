8dcbd6b715c6eeef0d3cca0d6538be0600afb450
Kasun Kumara <kasun.kumara@digital.cabinet-office.gov.uk>
2017-11-29 12:07:51 +0000

    PP-2996 allow deploy to specify smoke test category

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,14 +1,21 @@
 #!/usr/bin/env groovy
 
-def call(String microservice, String aws_profile, String tag = null, boolean tagAfterDeployment = false, boolean run_tests = true) {
+def call(String microservice, String aws_profile, String tag = null, boolean tagAfterDeployment = false, boolean run_tests = true, smoke_tags = null) {
     tag = tag ?: gitCommit()
-    
+
+    smoke_version = 'v1'
+    if (tag != null) {
+        smoke_version = 'v2'
+    }
+
     build job: 'deploy-pipeline-microservice',
         parameters:[
           string(name: 'MICROSERVICE', value: microservice),
           string(name: 'CONTAINER_VERSION', value: tag),
           string(name: 'AWS_PROFILE', value: aws_profile),
           booleanParam(name: 'TAG_AFTER_DEPLOYMENT', value: tagAfterDeployment),
-          booleanParam(name: 'RUN_TESTS', value: run_tests)
+          booleanParam(name: 'RUN_TESTS', value: run_tests),
+          booleanParam(name: 'SMOKE_VERSION', value: smoke_version),
+          booleanParam(name: 'SMOKE_TAGS', value: smoke_tags)
         ]
 }
