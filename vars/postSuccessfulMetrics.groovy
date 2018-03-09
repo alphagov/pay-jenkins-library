fd37980635e58def0a1db1284fa43e9e89c8b371
Belinda Cowey <belinda.cowey@digital.cabinet-office.gov.uk>
2018-03-09 14:09:34 +0000

    PP-3381 Instrument all stages of a master build and deployment to Test
    Adapt the function to accept an optional start time argument
    
    solo @belindac

--- vars/postSuccessfulMetrics.groovy
+++ vars/postSuccessfulMetrics.groovy
@@ -1,5 +1,24 @@
-def call(String metricPrefix) {
-    def long duration = (System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000;
+/*
+Post multiple metrics
+- metricPrefix defines what you want as the metric identifier
+  e.g. metricPrefix = 'connector.maven-build'
+       ci.ci-4.connector.maven-build.time
+       ci.ci-4.connector.maven-build.success
+- startTime defines start time of item you are timing
+  e.g. startTimeInMillis = -1
+       This will use current time - current build start time
+  e.g. startTimeInMillis = xxxxx
+       This will use current time - xxxx
+*/
+def call(String metricPrefix, long startTimeInMillis = -1) {
+    def long duration = 0
+
+    if (startTimeInMillis == -1) {
+      duration = (System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000;
+    } else {
+      duration = (System.currentTimeMillis() - startTimeInMillis) / 1000;
+    }
+
     postMetric("${metricPrefix}.time", duration, "new")
     postMetric("${metricPrefix}.success", 1, "new")
 }
