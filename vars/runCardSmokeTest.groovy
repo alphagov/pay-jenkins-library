b8aa47ca62d21631c329db5b1d8801b3c896f616
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2018-03-20 14:13:22 +0000

    PP-3527 Fix smoke test calls
    There is now a base runSmokeTest library fn that calls
    the `run-smoke-test` Jenkins job. That job uses the new
    version of the smoke script (run-smokev2) if you pass it
    `SMOKE_TAGS`. So all this works together, although a tad
    complicated to trace through. Once we've transitioned over
    we can delete all the old stuff and it will be clearer.

--- vars/runCardSmokeTest.groovy
+++ vars/runCardSmokeTest.groovy
@@ -2,8 +2,8 @@
 
 def call(String aws_profile = "test", boolean promoted_env = true) {
   runSmokeTest(
-          smoke_tags: "uk.gov.pay.endtoend.categories.SmokeCardPayments",
-          aws_profile: aws_profile,
-          promoted_env: promoted_env
+          "uk.gov.pay.endtoend.categories.SmokeCardPayments",
+          aws_profile,
+          promoted_env
   )
 }
