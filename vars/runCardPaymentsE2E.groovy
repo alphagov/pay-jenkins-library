4d0056c7a2c3c45539aecfafcf04546e7a861cb4
maxcbc <maxcbc@live.com>
2018-03-21 21:02:54 +0000

    PP-3436 allow setting of pay-scripts branch in separate e2e, accept and zap jobs

--- vars/runCardPaymentsE2E.groovy
+++ vars/runCardPaymentsE2E.groovy
@@ -1,18 +1,17 @@
 #!/usr/bin/env groovy
 
-def call(
-        String app,
-        String tag = null,
-        String jobName = 'run-end-to-end-card-payment-tests') {
+def call(String app, String tag = null, String pay_scripts_branch = 'master') {
 
     if (tag == null) {
         commit = env.GIT_COMMIT ?: gitCommit()
         tag = "${commit}-${env.BUILD_NUMBER}"
     }
 
-    build job: jobName,
+    build job: 'run-end-to-end-card-payment-tests',
             parameters: [
                     string(name: 'MODULE_NAME', value: app),
-                    string(name: 'MODULE_TAG', value: tag)
+                    string(name: 'MODULE_TAG', value: tag),
+                    string(name: 'PAY_SCRIPTS_BRANCH', value: pay_scripts_branch)
             ]
+
 }
