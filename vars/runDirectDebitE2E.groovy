94c39c35f677e04015cdbb355dad08e92bd71554
maxcbc <maxcbc@live.com>
2018-03-22 15:11:32 +0000

    PP-3436 when app = 'scripts' use git head as scripts branch

--- vars/runDirectDebitE2E.groovy
+++ vars/runDirectDebitE2E.groovy
@@ -3,11 +3,16 @@
 def call(
         String app = null,
         String tag = null,
-        String pay_scripts_branch = 'master') {
+        String pay_scripts_branch = null) {
+
+    commit = env.GIT_COMMIT ?: gitCommit()
 
     if (tag == null) {
-        commit = env.GIT_COMMIT ?: gitCommit()
         tag = "${commit}-${env.BUILD_NUMBER}"
+    }
+
+    if (app == 'scripts' && pay_scripts_branch == null) {
+        pay_scripts_branch = commit
     }
 
     build job: 'run-end-to-end-direct-debit-tests',
