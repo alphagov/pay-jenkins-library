5b7a03ce07d0423f6a4193d5b97c7dd415df1dae
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2018-05-08 14:24:34 +0100

    PP-3627: Script for running provider contract tests
    This script runs in the context of a project build, hence there is no need to
    re-checkout the repo and the call to gitCommit just works.
    
    @oswaldquek

---
+++ vars/runProviderContractTests.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+def call() {
+
+    echo 'Running provider contract tests'
+    def commit = gitCommit()
+    withCredentials([
+            string(credentialsId: 'pact_broker_username', variable: 'PACT_BROKER_USERNAME'),
+            string(credentialsId: 'pact_broker_password', variable: 'PACT_BROKER_PASSWORD')]
+    ) {
+        sh "mvn test -DrunContractTests -DPACT_CONSUMER_TAG=master -DPACT_BROKER_USERNAME=${PACT_BROKER_USERNAME} -DPACT_BROKER_PASSWORD=${PACT_BROKER_PASSWORD} -DPROVIDER_SHA=${commit}"
+    }
+}
