560ac1b23c2f98ec71c1561c35ebfd2efba6b9da
Danail <DanailMinchev@users.noreply.github.com>
2018-05-30 12:21:19 +0100

    Revert "PP-3776 Temporarily disable direct debit smoke tests"

--- vars/runDirectDebitSmokeTest.groovy
+++ vars/runDirectDebitSmokeTest.groovy
@@ -1,12 +1,9 @@
 #!/usr/bin/env groovy
 
 def call(String aws_profile = "test", boolean promoted_env = true) {
-  //Temporarily disabling to unblock pipeline, to allow diagnosis of 
-  //DD connection problems
-
-  //runSmokeTest(
-  //        "uk.gov.pay.endtoend.categories.SmokeDirectDebitPayments",
-  //        aws_profile,
-  //        promoted_env
-  //)
+  runSmokeTest(
+          "uk.gov.pay.endtoend.categories.SmokeDirectDebitPayments",
+          aws_profile,
+          promoted_env
+  )
 }
