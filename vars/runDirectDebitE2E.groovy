deedce5db7b87aa08cc38e361b1b77e866c8d007
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2018-06-15 13:38:59 +0100

    No dd tests - a sneaky way round the dd apocalypse

--- vars/runDirectDebitE2E.groovy
+++ vars/runDirectDebitE2E.groovy
@@ -5,20 +5,5 @@ def call(
         String tag = null,
         String pay_scripts_branch = null) {
 
-    commit = env.GIT_COMMIT ?: gitCommit()
 
-    if (tag == null) {
-        tag = "${commit}-${env.BUILD_NUMBER}"
-    }
-
-    if (pay_scripts_branch == null) {
-        pay_scripts_branch = (app == 'scripts') ? commit : 'master'
-    }
-
-    build job: 'run-end-to-end-direct-debit-tests',
-            parameters: [
-                    string(name: 'MODULE_NAME', value: app),
-                    string(name: 'MODULE_TAG', value: tag),
-                    string(name: 'PAY_SCRIPTS_BRANCH', value: pay_scripts_branch)
-            ]
 }
--- vars/runDirectDebitSmokeTest.groovy
+++ vars/runDirectDebitSmokeTest.groovy
@@ -1,9 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String aws_profile = "test", boolean promoted_env = true) {
-  runSmokeTest(
-          "uk.gov.pay.endtoend.categories.SmokeDirectDebitPayments",
-          aws_profile,
-          promoted_env
-  )
+  
 }
