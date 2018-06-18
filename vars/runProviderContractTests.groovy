9f6a0f942a3f997d682106221725f43509512312
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2018-06-18 11:04:00 +0100

    bau: Specify a flag to publish verification results to the pact broker
    We have (or will be upgrading) java projects to use version 3.5.18 of the pact
    jvm provider library, which disables publishing of verification result to the
    pact broker by default. See https://github.com/DiUS/pact-jvm/issues/695. We
    want to do this so results don't get accidentally published from development
    machines.So we have to set the pact.verifier.publishResults to true here.
    
    @oswaldquek

--- vars/runProviderContractTests.groovy
+++ vars/runProviderContractTests.groovy
@@ -8,6 +8,6 @@ def call() {
             string(credentialsId: 'pact_broker_username', variable: 'PACT_BROKER_USERNAME'),
             string(credentialsId: 'pact_broker_password', variable: 'PACT_BROKER_PASSWORD')]
     ) {
-        sh "mvn test -DrunContractTests -DPACT_CONSUMER_TAG=master -DPACT_BROKER_USERNAME=${PACT_BROKER_USERNAME} -DPACT_BROKER_PASSWORD=${PACT_BROKER_PASSWORD} -DPROVIDER_SHA=${commit}"
+        sh "mvn test -DrunContractTests -DPACT_CONSUMER_TAG=master -DPACT_BROKER_USERNAME=${PACT_BROKER_USERNAME} -DPACT_BROKER_PASSWORD=${PACT_BROKER_PASSWORD} -DPROVIDER_SHA=${commit} -Dpact.verifier.publishResults=true"
     }
 }
