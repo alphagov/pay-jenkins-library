7201c09cb738a7cf56d830594386d06ec637369e
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2019-01-04 10:28:10 +0000

    bau: Use correct env var to provide the provider version
    The provider version set by the PROVIDER_SHA env var is actually a custom Pay
    env var. The correct env var used by the pact libraries is
    pact.provider.version.
    
    @oswaldquek

--- vars/runProviderContractTests.groovy
+++ vars/runProviderContractTests.groovy
@@ -8,6 +8,8 @@ def call() {
             string(credentialsId: 'pact_broker_username', variable: 'PACT_BROKER_USERNAME'),
             string(credentialsId: 'pact_broker_password', variable: 'PACT_BROKER_PASSWORD')]
     ) {
-        sh "mvn test -DrunContractTests -DPACT_CONSUMER_TAG=master -DPACT_BROKER_USERNAME=${PACT_BROKER_USERNAME} -DPACT_BROKER_PASSWORD=${PACT_BROKER_PASSWORD} -DPROVIDER_SHA=${commit} -Dpact.verifier.publishResults=true"
+        sh "mvn test -DrunContractTests -DPACT_CONSUMER_TAG=master -DPACT_BROKER_USERNAME=${PACT_BROKER_USERNAME} " +
+                "-DPACT_BROKER_PASSWORD=${PACT_BROKER_PASSWORD} -DPROVIDER_SHA=${commit} -Dpact.provider.version=${commit} " +
+                "-Dpact.verifier.publishResults=true"
     }
 }
