9df58e68ce603cd3c7f6d219e66d8aa347543e4e
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-08-07 15:24:24 +0100

    Use the single variant of the end to end job

--- vars/runProductsE2E.groovy
+++ vars/runProductsE2E.groovy
@@ -4,5 +4,5 @@ def call(
         String app = null,
         String tag = null,
         String pay_scripts_branch = null) {
-    runAppE2E(app, 'run-end-to-end-products-tests', tag, pay_scripts_branch)
+    runAppE2E(app, 'products', tag, pay_scripts_branch)
 }
