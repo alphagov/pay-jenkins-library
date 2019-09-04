4e7cedbc29a4a4691415dea69571c7dccd79b09b
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-09-04 15:48:11 +0100

    Hardcode arguments to runSmokeTest
    We never call run*SmokeTest with any arguments, so remove them and hardcode the
    values instead.

--- vars/runSmokeTest.groovy
+++ vars/runSmokeTest.groovy
@@ -1,11 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(String smoke_tags, String aws_profile = "test", boolean promoted_env = false) {
-
+def call(String smoke_tags) {
   build job: 'run-smoke-test',
     parameters: [
-      string(name: 'AWS_PROFILE', value: aws_profile),
-      booleanParam(name: 'PROMOTED_ENV', value: promoted_env),
+      string(name: 'AWS_PROFILE', value: 'test'),
+      booleanParam(name: 'PROMOTED_ENV', value: true),
       string(name: 'SMOKE_TAGS', value: smoke_tags)
     ]
 }
