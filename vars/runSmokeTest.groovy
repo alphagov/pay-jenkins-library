ca044c24b739b57369a63dfb3363b45f21c23e41
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-09-06 11:26:41 +0100

    Remove extra parameters from run-smoke-test call
    The defaults for this job are fine, we don't need to pass in these extra
    parameters.

--- vars/runSmokeTest.groovy
+++ vars/runSmokeTest.groovy
@@ -3,8 +3,6 @@
 def call(String smoke_tags) {
   build job: 'run-smoke-test',
     parameters: [
-      string(name: 'AWS_PROFILE', value: 'test'),
-      booleanParam(name: 'PROMOTED_ENV', value: true),
       string(name: 'SMOKE_TAGS', value: smoke_tags)
     ]
 }
