bc88c78c0c3a859a53ce6aff0439a5e8197c6911
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2022-03-03 12:24:16 +0000

    PP-9153 Add run test command to multistage app build
    This was previously contained in Dockerfile, but makes
    things more flexible if tests are invoked by external
    call, in case e.g. of need to build without tests

--- vars/buildMultistageAppWithMetrics.groovy
+++ vars/buildMultistageAppWithMetrics.groovy
@@ -24,11 +24,9 @@ def call(body) {
     }
 
     def buildImageName = "build-${app}-${version}"
-    def testImageName = "test-${app}-${version}"
     def imageName = "${registry}/${docker_repo}/${app}"
 
     sh "docker build --target=builder -t ${buildImageName} ."
-    sh "docker build --target=tester  -t ${testImageName} ."
     withCredentials([
         string(credentialsId: 'pact_broker_username', variable: 'PACT_BROKER_USERNAME'),
         string(credentialsId: 'pact_broker_password', variable: 'PACT_BROKER_PASSWORD')]
@@ -39,14 +37,14 @@ def call(body) {
         " --env PACT_BROKER_USERNAME=${PACT_BROKER_USERNAME}" +
         " --env PACT_BROKER_PASSWORD=${PACT_BROKER_PASSWORD}" +
         " --env PACT_CONSUMER_TAG=${branch_name}" +
-        " ${testImageName} npm run publish-pacts"
+        " ${buildImageName} /bin/sh -c 'npm test  --forbid-only --forbid-pending && npm run publish-pacts'"
     }
 
     //copy out node modules for use by cypress container \o/
     sh "docker create --name ${buildImageName} ${buildImageName}"
     sh "docker cp ${buildImageName}:/app/node_modules ./node_modules"
 
-    sh "docker build --target=final ${build_flags} -t ${imageName}:${version} ."
+    sh "docker build ${build_flags} -t ${imageName}:${version} ."
 
     Date stopTime = new Date()
     long buildDiff = (stopTime.getTime() - startTime.getTime()) / 1000;
